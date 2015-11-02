67dfd8750dff51baf210036f2d868840b7367307
Ramsey <rdarling42@gmail.com>
2015-11-02 15:52:32 -0500

    hi res hero image, working slideshow, started animation for interactive boxes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(document).ready(function(){
+    $('#cities_slideshow').slippry(
+        {
+            'captions' : false,
+            'pager' : false,
+        } 
+        );
+});
